4af92853c916aeeb9b672f59336435576ccc9d73
Alan Mulhall <alanmulhall@hotmail.com>
2014-03-24 23:16:54 -0700

    typo

--- project_code/selfie/config/routes.js
+++ project_code/selfie/config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
-  }
+  },
 
   '/callback/index': {
     controller: 'callback',
